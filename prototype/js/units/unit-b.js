e23d6f140f10d5de6940886b97b583098725cb63
ykhaustoryofficial <mbcgame1101@gmail.com>
2026-09-22 08:58:10 +0900

    unit-b.js 업데이트

--- prototype/js/units/unit-b.js
+++ prototype/js/units/unit-b.js
@@ -1,89 +1,157 @@
-export const UNIT_B = {
+import{opening as O}from"../opening-presets.js";
+
+export const UNIT_B={
   id:"055.9500B",
   code:"B",
   area:55.9500,
 
+  /*
+    B형 실제 비율 재설정
+
+    WIDTH  = 12.805m
+    DEPTH  =  9.230m
+    HEIGHT =  2.850m
+
+    LOCAL
+    FRONT = +Z = 거실창 방향
+    REAR  = -Z
+    LEFT  = -X
+    RIGHT = +X
+  */
   size:{
-    width:9.40,
-    depth:7.00,
+    width:12.805,
+    depth:9.23,
     floorHeight:2.85,
-    wall:0.18
+    wall:.18
   },
 
-  /* 정면 오른쪽 안방 매스만 뒤로 후퇴 */
+  /*
+    FRONT
+
+    외부에서 왼쪽 → 오른쪽
+
+    거실
+    침실2
+    침실3
+    침실1
+
+    오른쪽 침실1 부분은
+    약 0.85m 뒤로 후퇴.
+  */
   frontProfile:[
     {
-      xMin:-4.70,
-      xMax:2.15,
+      xMin:-6.4025,
+      xMax:2.78,
       offset:0
     },
     {
-      xMin:2.15,
-      xMax:4.70,
-      offset:-0.90
+      xMin:2.78,
+      xMax:6.4025,
+      offset:-.85
     }
   ],
 
   frontOpenings:[
-    {
+
+    O("livingRoomWindow",{
       id:"F01",
       name:"livingRoomWindow",
-      type:"largeWindow",
-      x:-3.25,
-      w:1.95,
-      h:1.45,
-      sill:.62
-    },
-    {
+      x:-4.20,
+      w:2.65
+    }),
+
+    O("smallRoomWindow",{
       id:"F02",
-      name:"smallRoomWindow1",
-      type:"window",
-      x:-1.15,
-      w:1.05,
-      h:1.10,
-      sill:.97
-    },
-    {
+      name:"bedroom2Window",
+      x:-1.20,
+      w:1.57
+    }),
+
+    O("smallRoomWindow",{
       id:"F03",
-      name:"smallRoomWindow2",
-      type:"window",
-      x:.75,
-      w:1.10,
-      h:1.10,
-      sill:.97
-    },
-    {
+      name:"bedroom3Window",
+      x:1.27,
+      w:1.57
+    }),
+
+    O("masterRoomWindow",{
       id:"F04",
-      name:"masterRoomWindow",
-      type:"window",
-      x:3.15,
-      w:1.35,
-      h:1.35,
-      sill:.72
-    }
+      name:"bedroom1Window",
+      x:4.34,
+      w:1.96
+    })
+
   ],
 
+  /*
+    REAR PROFILE
+
+    이미지 평면 외곽을 비례 환산한 값.
+
+    가장 왼쪽 후면부가 기준점이고
+    중앙부가 단계적으로 앞으로 들어옴.
+  */
+  rearProfile:[
+
+    {
+      xMin:-6.4025,
+      xMax:-2.87,
+      offset:0
+    },
+
+    {
+      xMin:-2.87,
+      xMax:.40,
+      offset:2.08
+    },
+
+    {
+      xMin:.40,
+      xMax:2.05,
+      offset:3.13
+    },
+
+    {
+      xMin:2.05,
+      xMax:6.4025,
+      offset:1.97
+    }
+
+  ],
+
+  /*
+    B형 REAR에는
+    현재 주요 외부 창호 없음.
+  */
   rearOpenings:[],
 
+  /*
+    LEFT
+
+    도면 위쪽(후면) → 아래쪽(전면)
+
+    실외기실 루버
+    주방창
+
+    z=-4.615 = 최후면
+    z=+4.615 = 최전면
+  */
   leftOpenings:[
-    {
+
+    O("outdoorUnitLouver",{
       id:"L01",
       name:"outdoorUnitLouver",
-      type:"louver",
-      z:-2.45,
-      w:.78,
-      h:1.45,
-      sill:.68
-    },
-    {
+      z:-3.39,
+      w:.95
+    }),
+
+    O("kitchenWindow",{
       id:"L02",
       name:"kitchenWindow",
-      type:"kitchenWindow",
-      z:-1.10,
-      w:.72,
-      h:.56,
-      sill:1.24
-    }
+      z:-.91,
+      w:.95
+    })
+
   ],
 
   rightOpenings:[]
