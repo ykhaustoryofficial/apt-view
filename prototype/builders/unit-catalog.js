73294e5a82df0fe2fa3d93176c36748d6ef21472
ykhaustoryofficial <mbcgame1101@gmail.com>
2026-09-22 12:49:00 +0900

    unit-catalog.js 업데이트

--- prototype/builders/unit-catalog.js
+++ prototype/builders/unit-catalog.js
@@ -1,9 +1,9 @@
-import{UNIT_A}from"../units/unit-a.js";
-import{UNIT_B}from"../units/unit-b.js";
-import{UNIT_C}from"../units/unit-c.js";
-import{UNIT_D}from"../units/unit-d.js";
-import{UNIT_E}from"../units/unit-e.js";
-import{UNIT_F}from"../units/unit-f.js";
+import{UNIT_A}from"../js/units/unit-a.js";
+import{UNIT_B}from"../js/units/unit-b.js";
+import{UNIT_C}from"../js/units/unit-c.js";
+import{UNIT_D}from"../js/units/unit-d.js";
+import{UNIT_E}from"../js/units/unit-e.js";
+import{UNIT_F}from"../js/units/unit-f.js";
 
 export const UNIT_CATALOG=Object.freeze({
   A:UNIT_A,
